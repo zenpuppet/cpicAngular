078661076c694c6c978a16ff0601684b7a38007e
ZenPuppet <todd@mediaspiders.com>
2018-01-30 10:48:15 -0500

    Added a list module with list components

---
+++ src/app/list/list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IssueComponent } from './issue/issue.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ValidationComponent } from './validation/validation.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [IssueComponent, ScheduleComponent, ValidationComponent, DictionaryComponent]
+})
+export class ListModule { }
