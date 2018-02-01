36f30a1b503f01487031640e88478ea4a65bd0c7
ZenPuppet <todd@mediaspiders.com>
2018-02-01 09:32:56 -0500

    Added the MenuComponent and FooterComponent to the Core Module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainnavComponent } from './mainnav/mainnav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MenuComponent, FooterComponent]
+  declarations: [MenuComponent, FooterComponent, MainnavComponent],
+  exports: [MenuComponent, FooterComponent, MainnavComponent]
+
 })
 export class CoreModule { }
