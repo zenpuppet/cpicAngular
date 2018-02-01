929c3eeb48de6e1fe4caaacc9145f2f6a7294ed1
ZenPuppet <todd@mediaspiders.com>
2018-02-01 10:37:32 -0500

    Added a main nav component

---
+++ src/app/modules/core/mainnav/mainnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpic-mainnav',
+  templateUrl: './mainnav.component.html',
+  styleUrls: ['./mainnav.component.css']
+})
+export class MainnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
