0f8baf54aec866c73f73a8d93507f8f057e097c3
Scott McCaughey <scott@scottmccaughey.com>
2018-03-07 14:41:39 -0500

    Visual tweaks

--- src/app/modules/list/validation/validation.component.ts
+++ src/app/modules/list/validation/validation.component.ts
@@ -5,7 +5,7 @@ import { ApiService } from '../../data/api.service';
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
-  styleUrls: ['./validation.component.css']
+  styleUrls: ['./validation.component.scss']
 })
 export class ValidationComponent implements OnInit {
 
